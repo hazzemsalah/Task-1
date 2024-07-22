0d52b5a100965c16e9ea5ecffb19bc86744aabbe
hazzem salah <hazzemsalaht6@gmail.com>
2024-07-22 13:45:55 +0300

    implement Service Locator Pattern with get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task/cubits/post_cubit.dart';
+import 'package:task/locator.dart';
 import 'package:task/pages/home_page.dart';
 import 'package:task/services/post_service.dart';
 
 void main() {
+  setupLocator();
   runApp(const PostsApp());
 }
 
